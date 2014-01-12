cceae5b5dfa641ee1b341d835bf522049ebdcde4
Purlox <purlox@gmail.com>
2014-01-12 22:12:53 +0100

    Added warnings

---
+++ source/warnings.c
@@ -0,0 +1,17 @@
+error_flag overwrite_file_message(char *file_name) {
+  char answer;
+
+  printf("Do you want to overwrite file %s ? [Y/N] ", file_name);
+
+scan_for_answer:
+  scanf("%c", &answer);
+
+  if(answer == 'Y' || answer == 'y') {
+    return true;
+  } else if(answer == 'N' || answer == 'n') {
+    return false
+  } else {
+    printf("You can only answer with Y or N. Try again: ");
+    goto scan_for_answer;
+  }
+}
---
+++ source/warnings.h
@@ -0,0 +1,7 @@
+#ifndef WARNINGS_H_INCLUDED
+#define WARNINGS_H_INCLUDED
+
+// Asks if to overwrite a file while saving
+error_flag overwrite_file_message(char *file_name) ;
+
+#endif // WARNINGS_H_INCLUDED
