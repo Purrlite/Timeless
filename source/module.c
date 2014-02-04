9095afc78e8b28008d438a6f0958f3b187c57b49
Purlox <purlox@gmail.com>
2014-02-04 18:12:31 +0100

    Added open and close functions

--- source/module.c
+++ source/module.c
@@ -1,2 +1,35 @@
 #include "module.h"
+#include "errors.h"
 
+module_s * module_open(char *path, char *name, char *modification_abbr) {
+  char *full_filename = malloc(512);
+  module_s * new_module = malloc( sizeof(module_s) );
+
+  strncpy(full_filename, path, 500);
+  strcat(full_filename, name);
+  strcat(full_filename, modification_abbr);
+
+  new_module.module = LoadLibrary(full_filename);
+
+  if(new_module.module == NULL)
+    return NULL;
+
+  new_module.name = name;
+  new_module.modification_abbr = modification_abbr;
+
+  return new_module;
+}
+
+error_flag module_close(module_s * module) {
+  if(module == NULL)
+    return BAD_FUNCTION_ARGUMENT;
+
+  if(FreeLibrary(module.module) == 0)
+    return IO_ERROR;
+
+  free(module.name);
+  free(module.modification_abbr);
+  free(module);
+
+  return SUCCESS;
+}
--- source/module.h
+++ source/module.h
@@ -2,16 +2,22 @@
 #define MODULE_H_INCLUDED
 
 #include <windows.h>
-
-typedef struct {
-  HMODULE *modules;
-  char **names;
-  int number_of_loaded_modules;
-} module_list_s;
+#include <string.h>
+#include <stdlib.h>
 
 typedef struct {
   HMODULE module;
-  char name;
+  char *name;
+  char *modification_abbr; // Abbreviation of the name of the modification the module belongs to
 } module_s;
+
+typedef struct {
+  module_s *modules;
+  int number_of_loaded_modules;
+} module_list_s;
+
+
+module_s * module_open(char *path, char *name, char *modification_name) ;
+void module_close(module_s module) ;
 
 #endif // MODULE_H_INCLUDED
