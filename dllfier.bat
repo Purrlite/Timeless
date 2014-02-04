1bc1578a3821d8a66a431800d29288b5059b61ea
Purlox <purlox@gmail.com>
2014-02-04 11:14:57 +0100

    Made a small change

--- dllfier.bat
+++ dllfier.bat
@@ -2,6 +2,7 @@
 ::  - second argument is for optional arguments for gcc compilation
 
 @echo off
+
 if /I %~x1 == .o  call gcc -shared -o %~n1.dll %1
 
 if /I %~x1 == .c  call gcc -c -std=c11 %1 %~2
