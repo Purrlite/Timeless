1e60f3d655ddc0f0781ae8edba163f57fb4811a1
Purlox <purlox@gmail.com>
2014-02-04 10:25:32 +0100

    Improved the dllfier so only one is required

--- dllfier (from .o).bat
+++
@@ -1,2 +0,0 @@
-:: Makes a .dll file from .o file. Deprecated.
-call gcc -shared -o %~n1.dll %1
--- dllfier.bat
+++ dllfier.bat
@@ -1,5 +1,9 @@
 :: Makes a .dll file from .c file.
 ::  - second argument is for optional arguments for gcc compilation
-call gcc -c %1 %~2
-call gcc -shared -o %~n1.dll %~n1.o
-del %~n1.o
+
+@echo off
+if %~x1 == .o  call gcc -shared -o %~n1.dll %1
+
+if %~x1 == .c  call gcc -c -std=c11 %1 %~2
+if %~x1 == .c  call gcc -shared -o %~n1.dll %~n1.o
+if %~x1 == .c  del %~n1.o
