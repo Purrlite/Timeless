a9fbacbddf2ab740ad6842ec2d7a536e287de395
Purlox <purlox@gmail.com>
2014-02-04 10:29:14 +0100

    Made it case insensitive

--- dllfier.bat
+++ dllfier.bat
@@ -2,8 +2,8 @@
 ::  - second argument is for optional arguments for gcc compilation
 
 @echo off
-if %~x1 == .o  call gcc -shared -o %~n1.dll %1
+if /I %~x1 == .o  call gcc -shared -o %~n1.dll %1
 
-if %~x1 == .c  call gcc -c -std=c11 %1 %~2
-if %~x1 == .c  call gcc -shared -o %~n1.dll %~n1.o
-if %~x1 == .c  del %~n1.o
+if /I %~x1 == .c  call gcc -c -std=c11 %1 %~2
+if /I %~x1 == .c  call gcc -shared -o %~n1.dll %~n1.o
+if /I %~x1 == .c  del %~n1.o
